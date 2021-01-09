401aa2384bd5447d19f66e3e2ad479b7567e05fc
token-hub <63064941+token-hub@users.noreply.github.com>
2021-01-09 17:32:09 +0800

    sanctum initial functions working

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,10 +15,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/test', function () {
+        return User::all();
+    });
+
+    Route::post('/test', function () {
+        return response()->json('yey');
+    });
 });
 
 Route::get('/test', function () {
-    return ['name' => 'Hurrayy'];
+    return User::all();
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+
+    return ['token' => $token->plainTextToken];
 });
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
